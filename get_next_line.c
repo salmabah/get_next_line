630c78c5ca436abd57bf7dd4f5a85d156bc71d8c
Salma Bahraoui <sbahraou@e2r8p6.1337.ma>
2022-03-16 00:08:12 +0100

    gnl

--- get_next_line.c
+++ get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: sbahraou <sbahraou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/13 16:48:37 by sbahraou          #+#    #+#             */
-/*   Updated: 2022/03/13 22:56:12 by sbahraou         ###   ########.fr       */
+/*   Updated: 2022/03/15 23:48:31 by sbahraou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,55 +14,99 @@
 #include <string.h>
 #include <stdlib.h>
 
-char *cutbuffer(char *buffer, int *nb_bytes)
+char *spilt(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i] != '\n' && str[i] != '\0')
+		i++;
+	str[i] = '\0';
+	return (str);
+}
+
+char *cutbuffer(char **buffer, int *nb_bytes, int fd, int cnt)
 {
 	char	*ligne;
+	char	*temp;
 	int		i;
 
 	// if (ft_strchr(buffer, '\n') == NULL)
 	// {
 	// 	ligne = ft_strdup("");
 	// }
-
 	i = 0;
 	while (buffer[i] != '\n' && buffer[i])
 		i++;
-
-	ligne = ft_substr(buffer, 0, i);
-	return (ligne);
+	// ligne = ft_substr(buffer, 0, i);
+	// if (i == BUF_SIZE)
+	// {
+		temp = *buffer;
+		printf("cnt : %d", cnt);
+		printf("\nold buffer : %s\n", temp);
+		*buffer = malloc(BUF_SIZE * cnt);
+		ligne = malloc(BUF_SIZE);
+		if (ligne != NULL && buffer != NULL)
+		{
+			*nb_bytes = read(fd, ligne, BUF_SIZE);
+			*buffer = ft_strjoin(temp, ligne);
+			printf("\nnew buffer : %s\n", *buffer);
+			// *nb_bytes = 0;
+		}
+		// buffer = malloc(strlen(temp) + BUF_SIZE);
+		// buffer = ft_strjoin(temp, ligne);
+	// }
+	return (*buffer);
 }
 
-char    *get_next_line(int fd)
+char	*get_next_line(int fd)
 {
 	int		resultat;
 	char	*buffer;
 	char	*ligne;
 	int		nb_bytes;
+	int		cnt;
 
-	buffer = (char *)malloc(sizeof(char) * BUF_SIZE + 1);
+	cnt = 1;
+	if ((buffer = (char *)malloc(sizeof(char) * BUF_SIZE + 1)) == NULL)
+		return (NULL);
     nb_bytes = read(fd, buffer, BUF_SIZE);
-	printf("%s %d", buffer, nb_bytes);
+	printf("\n%s %d", buffer, nb_bytes);
 	while (nb_bytes > 0)
 	{
-		ligne = cutbuffer(buffer, &nb_bytes);
+		cnt++;
+		ligne = cutbuffer(buffer, &nb_bytes, fd, cnt);
 	}
-    return (ligne);
+	return (ligne);
 }
 
 int main()
 {
 	FILE	*f;
 	int		fd;
-	char	*line;
+	// char	*line;
 
 	f = fopen("test.txt", "r");
 	fd = fileno(f);
-	printf("%d", fd);
-	line = get_next_line(fd);
-	// printf("%s", line);
+	// line = get_next_line(fd);
+	printf("%s", get_next_line(fd));
 	return (0);
 }
 
+// int main()
+// {
+// 	char ligne[25] = "Salma bahraoui";
+// 	printf("%s\n", ligne);
+// 	ligne[5]='\0';
+// 	printf("%s", ligne);
+// 	int i = 0;
+// 	char str[4] = {'b','o','n', '\0'};
+// 	printf("%s", str);
+// 	while (str[i] != '\0')
+// 		i++;
+// 	printf("%d", i);
+// 	return (0);
+// }
 // int	main()
 // {
 // 	char	buf[BUF_SIZE];
